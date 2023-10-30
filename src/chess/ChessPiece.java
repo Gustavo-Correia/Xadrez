5b2f24c5953b3e6af8878cc56c48efee202a3843
Gustavo-Correia <gucorreia2901@gmail.com>
2023-10-30 10:32:34 -0300

    decrease and increase movement

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,7 +4,8 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece{
     private Color color;
-
+    private int moveCount;
+    
     public ChessPiece(boardgame.board board, Color color) {
         super(board);
         this.color = color;
@@ -14,11 +15,24 @@ public abstract class ChessPiece extends Piece{
     public Color getColor() {
         return color;
     }
+
+    protected void increaseMoveCount() {
+		moveCount++;
+	}
+
+	protected void decreaseMoveCount() {
+		moveCount--;
+	}
     public ChessPosition getChessPosition(){
         return ChessPosition.fromPosition(position);
     }
     protected boolean isThereOpponentPiece(Position position){
         ChessPiece p = (ChessPiece)getBoard().piece(position);
         return p != null && p.getColor() != color;
+    }
+
+
+    public int getMoveCount() {
+        return moveCount;
     }
 }
